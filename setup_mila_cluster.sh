1016b575d1739831aae1c0122ac6b19f25ef9707
Moksh Jain <mokshjn00@gmail.com>
2024-04-04 20:07:32 -0400

    update packages

--- setup_mila_cluster.sh
+++ setup_mila_cluster.sh
@@ -1,21 +1,21 @@
-module load miniconda/3 cuda/11.7
-conda create -p $SLURM_TMPDIR/env python=3.9 -y
+module load miniconda/3 cudatoolkit/12.1
+conda create -p $SLURM_TMPDIR/env python=3.10 -y
 conda activate $SLURM_TMPDIR/env
 # only necessary for RNA task
 # conda install -c bioconda viennarna -y
 
 cd $SLURM_TMPDIR
-wget https://data.pyg.org/whl/torch-1.13.0%2Bcu117/torch_cluster-1.6.1%2Bpt113cu117-cp39-cp39-linux_x86_64.whl
-wget https://data.pyg.org/whl/torch-1.13.0%2Bcu117/torch_scatter-2.1.1%2Bpt113cu117-cp39-cp39-linux_x86_64.whl
-wget https://data.pyg.org/whl/torch-1.13.0%2Bcu117/torch_sparse-0.6.17%2Bpt113cu117-cp39-cp39-linux_x86_64.whl
-pip install torch_cluster-1.6.1+pt113cu117-cp39-cp39-linux_x86_64.whl
-pip install torch_scatter-2.1.1+pt113cu117-cp39-cp39-linux_x86_64.whl
-pip install torch_sparse-0.6.17+pt113cu117-cp39-cp39-linux_x86_64.whl
+wget https://data.pyg.org/whl/torch-2.1.0%2Bcu121/torch_cluster-1.6.3%2Bpt21cu121-cp310-cp310-linux_x86_64.whl
+wget https://data.pyg.org/whl/torch-2.1.0%2Bcu121/torch_scatter-2.1.2%2Bpt21cu121-cp310-cp310-linux_x86_64.whl
+wget https://data.pyg.org/whl/torch-2.1.0%2Bcu121/torch_sparse-0.6.18%2Bpt21cu121-cp310-cp310-linux_x86_64.whl
+pip install torch_cluster-1.6.3+pt21cu121-cp310-cp310-linux_x86_64.whl
+pip install torch_scatter-2.1.2+pt21cu121-cp310-cp310-linux_x86_64.whl
+pip install torch_sparse-0.6.18+pt21cu121-cp310-cp310-linux_x86_64.whl
 cd ~/gflownet
-pip install torch==1.13.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
-pip install -e . --find-links https://data.pyg.org/whl/torch-1.13.1+cu117.html
+pip install torch==2.1.2+cu121 --extra-index-url https://download.pytorch.org/whl/cu121
+pip install -e . --find-links https://data.pyg.org/whl/torch-2.1.0+cu121.html
 pip install PyTDC levenshtein wandb 
 pip install -U rliable
 # only necessary for RNA task
 # pip install flexs
-pip install ray==2.6.2
+pip install ray
